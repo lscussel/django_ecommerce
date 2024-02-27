0ccfa4efdb929765c9033e06197c00b8ed33ae9d
Leonardo Scussel <leonardoscussel@gmail.com>
2024-02-27 19:28:17 -0300

    created produto and variacao models

--- loja/settings.py
+++ loja/settings.py
@@ -39,6 +39,9 @@ INSTALLED_APPS = [
     'django.contrib.messages',
     'django.contrib.staticfiles',
 
+    # Meus apps
+    'produto',
+
     # Debug Toolbar
     'debug_toolbar',
 ]
--- produto/admin.py
+++ produto/admin.py
@@ -1,3 +1,16 @@
 from django.contrib import admin
+from .models import Produto, Variacao
+
 
 # Register your models here.
+class VariacaoInline(admin.TabularInline):
+  model = Variacao
+  extra = 1
+
+@admin.register(Produto)
+class ProdutoAdmin(admin.ModelAdmin):
+  inlines = [ VariacaoInline ]
+
+@admin.register(Variacao)
+class VariacaoAdmin(admin.ModelAdmin):
+  ...
