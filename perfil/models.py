8776ad423b2bb736dcd040892312eee37c26ef1e
Leonardo Scussel <leonardoscussel@gmail.com>
2024-02-27 21:53:04 -0300

    implemented clean method from perfil model

--- perfil/models.py
+++ perfil/models.py
@@ -1,14 +1,18 @@
 from django.db import models
 from django.contrib.auth.models import User
+from django.forms import ValidationError
+
+import re
+from utils.validacpf import valida_cpf
 
 # Create your models here.
 class Perfil(models.Model):
-  usuario = models.OneToOneField(User, on_delete=models.PROTECT)
+  usuario = models.OneToOneField(User, on_delete=models.PROTECT, verbose_name='usuário')
   data_nascimento = models.DateField()
   cpf = models.CharField(max_length=11)
   endereco = models.CharField(max_length=34)
   numero = models.CharField(max_length=8)
-  complemento = models.CharField(max_length=8)
+  complemento = models.CharField(max_length=8, blank=True)
   bairro = models.CharField(max_length=30)
   cep = models.CharField(max_length=8)
   cidade = models.CharField(max_length=30)
@@ -53,4 +57,13 @@ class Perfil(models.Model):
     return f'{self.usuario.first_name} {self.usuario.last_name}'
   
   def clean(self):
-    ...
+    error_messages = {}
+
+    if not valida_cpf(self.cpf):
+      error_messages['cpf'] = 'Digite um CPF válido'
+
+    if re.search(r'[^0-9]', self.cep) or len(self.cep) < 8:
+      error_messages['cep'] = 'CEP inválido'
+
+    if error_messages:
+      raise ValidationError(error_messages)
