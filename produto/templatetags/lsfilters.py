8772567f4a60c3addccc63432f52896361464dec
Leonardo Scussel <leonardoscussel@gmail.com>
2024-03-03 09:08:28 -0300

    finished removerdocarrinho view

--- produto/templatetags/lsfilters.py
+++ produto/templatetags/lsfilters.py
@@ -6,3 +6,7 @@ register = Library()
 @register.filter
 def formata_preco(valor):
   return utils.formata_preco(valor)
+
+@register.filter
+def total_itens_carrinho(carrinho):
+  return utils.total_itens_carrinho(carrinho)
--- utils/utils.py
+++ utils/utils.py
@@ -1,2 +1,5 @@
 def formata_preco(valor):
   return f'R$ {valor:.2f}'.replace('.', ',')
+
+def total_itens_carrinho(carrinho):
+  return sum([item['quantidade'] for item in carrinho.values()])
